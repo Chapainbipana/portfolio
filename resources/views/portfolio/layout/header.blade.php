2c0e06070bf3a952ff591ab990ea537d58e39f44
bipanachapain <bipanachapain.websoft@gmail.com>
2025-10-07 12:31:01 +0545

    the change the title image

--- resources/views/portfolio/layout/header.blade.php
+++ resources/views/portfolio/layout/header.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bipana Chapain</title>
+        <link rel="icon" type="image/png" href="{{ asset('assests/images/facvio.jpg') }}">
+
 
     <!-- Bootstrap CSS -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
